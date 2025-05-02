fd5f4d5b452d4dff24bbb0faefe767c1d68db9bb
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-02 19:24:13 +0200

    add header to admin #11

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../common/components/footer.component";
+import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [FooterComponent, RouterOutlet, MatToolbar, MatIcon, NgIf],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  title = 'TPV';
+  username = "jesus";
+  cartItemCount: number = 3;
+
+  login(): void {
+    // TODO
+  }
+
+  logout(): void {
+    // TODO
+  }
+
+  isAuthenticated(): boolean { //TODO
+    return true;
+  }
 
 }
