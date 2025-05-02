39a74f1840e9fcc8a6ba3a2d92056227d14db944
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-02 12:47:05 +0200

    add main routes #5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WellcomeComponent } from './wellcome/wellcome.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home/wellcome' },
+    {
+        path: 'home', component: HomeComponent,
+        children: [
+            { path: 'wellcome', component: WellcomeComponent }, // PUBLIC
+        ]
+    },
+    {
+        path: 'admin', component: AdminComponent,
+        children: []
+    }
+];
