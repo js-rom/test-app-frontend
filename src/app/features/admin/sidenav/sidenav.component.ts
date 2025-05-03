74254e1deae5ff7095948c71f4e4f63a002eccfb
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-03 13:35:50 +0200

    add matSideNav

--- src/app/features/admin/sidenav/sidenav.component.ts
+++ src/app/features/admin/sidenav/sidenav.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSidenavContainer } from '@angular/material/sidenav';
+import { MatSidenavContent } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatIcon, MatNavList, RouterLink, RouterOutlet],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
