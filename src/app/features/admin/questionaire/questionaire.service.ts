ad068e8306eeb870893b5cd899a5d2d5bc46dda0
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-03 17:52:56 +0200

    add questionaire service #1

---
+++ src/app/features/admin/questionaire/questionaire.service.ts
@@ -0,0 +1,15 @@
+import { Observable, of } from "rxjs";
+import { Questionaire } from "./questionaire.model";
+
+export class QuestionaireService {
+    
+    readAll() : Observable<Questionaire[]> {
+        return of([
+            {id: 1, description: 'cuestionario 1'},
+            {id: 2, description: 'cuestionario 2'},
+            {id: 3, description: 'cuestionario 3'},
+            {id: 4, description: 'cuestionario 4'},
+            {id: 5, description: 'cuestionario 5'},
+        ]);
+    }
+}
