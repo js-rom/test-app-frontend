05c2ddad2a38caca5df8df277ac6601f05b772b6
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-03 14:02:37 +0200

    .

--- src/app/features/admin/sidenav/sidenav.component.ts
+++ src/app/features/admin/sidenav/sidenav.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { MatSidenavContainer } from '@angular/material/sidenav';
-import { MatSidenavContent } from '@angular/material/sidenav';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatIcon } from '@angular/material/icon';
-import { MatNavList } from '@angular/material/list';
-import { RouterLink } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatIcon, MatNavList, RouterLink, RouterOutlet],
+  imports: [],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.css'
+  styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
 
