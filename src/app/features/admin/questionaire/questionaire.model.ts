c1c24fff8bcbedd51f5c1e2e2ba9d031762ac71b
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-03 19:35:18 +0200

    add main view of questionaire component #1

--- src/app/features/admin/questionaire/questionaire.model.ts
+++ src/app/features/admin/questionaire/questionaire.model.ts
@@ -1,4 +1,4 @@
 export interface Questionaire {
-    id: number;
+    id: string;
     description: string;
 }
--- src/app/features/admin/questionaire/questionaire.service.ts
+++ src/app/features/admin/questionaire/questionaire.service.ts
@@ -1,15 +1,21 @@
 import { Observable, of } from "rxjs";
 import { Questionaire } from "./questionaire.model";
+import { Injectable } from "@angular/core";
 
+@Injectable({providedIn: 'root'})
 export class QuestionaireService {
     
     readAll() : Observable<Questionaire[]> {
         return of([
-            {id: 1, description: 'cuestionario 1'},
-            {id: 2, description: 'cuestionario 2'},
-            {id: 3, description: 'cuestionario 3'},
-            {id: 4, description: 'cuestionario 4'},
-            {id: 5, description: 'cuestionario 5'},
+            {id: '1', description: 'cuestionario 1'},
+            {id: '2', description: 'cuestionario 2'},
+            {id: '3', description: 'cuestionario 3'},
+            {id: '4', description: 'cuestionario 4'},
+            {id: '5', description: 'cuestionario 5'},
         ]);
+    }
+
+    delete() {
+
     }
 }
