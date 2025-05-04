5d999853038508855010a1a84b5c755e2d020845
[jesus romero] <jesus.romerovidal@gmail.com>
2025-05-04 16:26:15 +0200

    add SingleselectionQuestion component #1

---
+++ src/app/features/admin/questionaire/new-questionaire/single-selection-question/single-selection-question.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-selection-question',
+  imports: [],
+  templateUrl: './single-selection-question.component.html',
+  styleUrl: './single-selection-question.component.css'
+})
+export class SingleSelectionQuestionComponent {
+
+}
